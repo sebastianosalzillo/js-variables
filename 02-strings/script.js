ce109dfdeeae69ff4184bde3bcad8e3d5fe4529c
sebastianosalzillo <salzillosebastiano@gmail.com>
2024-10-25 17:10:46 +0200

    feat: finito

---
+++ 02-strings/script.js
@@ -0,0 +1,32 @@
+// 1. Dichiarazione di variabili
+// Crea una variabile chiamata name e assegnale il tuo nome.
+// Stampa il valore di nome nella console.
+
+// 2. Tipi primitivi
+// Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+// un numero, una stringa e un valore booleano.
+// Stampa tutti i valori nella console.
+
+// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
+// e le variabili sopra in modo da stampare correttamente 
+// in console la stringa con il nome completo
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
+const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
+const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
+
+let name = 'sebastiano';
+console.log(name);
+
+let number = '1';
+let text = 'amala';
+let bool = '1010';
+
+console.log(number ,text ,1010 )
+
+const fullName = firstName + " " +lastName ;
+console.log(fullName); // Output: "Jane Smith"
+
+// 4. Imposta questa variabile utilizzando una proprietà
+// sulla variabile alphabet per ottenere la sua lunghezza
+const numberOfLettersInAlphabet = alphabet.length;
+console.log(numberOfLettersInAlphabet); // Output: 26
